6f4b2c66116358ea8367140120e58c2a9d0427a6
bryan <bryan.kouamo@institutsaintjean.org>
2026-04-23 15:04:03 +0100

    Address review feedback on dashboard perf PR

--- i18n/navigation.ts
+++ i18n/navigation.ts
@@ -1,7 +1,18 @@
+import { createElement, forwardRef, type ComponentProps, type ElementRef } from 'react';
 import { createNavigation } from 'next-intl/navigation';
 
 import { routing } from '@/i18n/routing';
 
-export const { Link, redirect, usePathname, useRouter, getPathname } =
-  createNavigation(routing);
+const navigation = createNavigation(routing);
+const NavigationLink = navigation.Link;
+
+export const Link = forwardRef<ElementRef<typeof NavigationLink>, ComponentProps<typeof NavigationLink>>(
+  function Link({ prefetch = false, ...props }, ref) {
+    return createElement(NavigationLink, { ...props, prefetch, ref });
+  },
+);
+
+Link.displayName = 'Link';
+
+export const { redirect, usePathname, useRouter, getPathname } = navigation;
 
